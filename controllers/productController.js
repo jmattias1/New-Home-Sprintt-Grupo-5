51283b82601fc6666a8e8ec97506d02d3192df67
Danimartinez25 <danimartinez1325@gmail.com>
2022-08-10 02:50:17 -0300

    Form hechos

--- controllers/productController.js
+++ controllers/productController.js
@@ -6,5 +6,15 @@ module.exports = {
         return res.render('productCart',{
             title : 'Carrito'
         })   
+    },
+    productAdd : (req,res) => {
+        return res.render('productAdd',{
+            title : 'Actualizar'
+        })   
+    },
+    productEdition: (req,res) => {
+        return res.render('productEdition',{
+            title : 'Edición'
+        })   
     }
 }
--- routes/products.js
+++ routes/products.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
-const {productDetail, productCart} = require('../controllers/productController')
+const {productDetail, productCart,productAdd,productEdition} = require('../controllers/productController')
 
 /*/products */
 router.get('/productDetail', productDetail);
 router.get('/productCart', productCart);
+router.get('/productAdd',productAdd);
+router.get('/productEdition',productEdition);
 
 
 module.exports = router;
