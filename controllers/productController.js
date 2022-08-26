1e0605c91958757e163d9ab7e56e53cd20752119
Danimartinez25 <danimartinez1325@gmail.com>
2022-08-26 10:50:22 -0300

    vista confirm

--- controllers/productController.js
+++ controllers/productController.js
@@ -67,17 +67,16 @@ controller = {
 		return res.render('productAdd')
 	},
 	store: (req, res) => {
-		const {price,section,discount,description,title,type} = req.body;
+		const {price,section,discount,description,title} = req.body;
         const products = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'data', 'products.json')));
-		const newProduct = {
+        const newProduct = {
             id : products[products.length - 1].id + 1,
-			title : title?.trim(),
-			description : description?.trim(),
+			title : title.trim(),
+			description : description.trim(),
 			price : +price,
 			discount : +discount,
 			image : 'default-img.png',
-			section,
-            type
+			section
 		}
 		let productModify = [...products, newProduct]
 		fs.writeFileSync(path.join(__dirname, '..', 'data', 'products.json'),JSON.stringify(productModify,null,3),'utf-8');    
@@ -87,6 +86,11 @@ controller = {
         return res.render('productCart',{
             title : 'Carrito'
         })   
+    },
+    remove : (req,res) => {
+        return res.render('confirm', {
+            id : req.params.id
+        })
     },
     destroy : (req,res) => {
         const {id} = req.params;
--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,7 @@
 var express = require('express');
 /* const productController = require('../controllers/productController'); */
 var router = express.Router();
-const {detail,all,edit,update,cart,store,create,destroy} = require('../controllers/productController')
+const {detail,all,edit,update,cart,store,create,destroy,remove} = require('../controllers/productController')
 
 /*/products */
 router    
@@ -12,7 +12,8 @@ router
     .put('/update/:id',update)
     .post('/store',store)
     .get('/create',create) 
-    .get('/cart',cart)     
-    .delete('/delete/:id',destroy)
+    .get('/cart',cart)  
+    .get('/delete/:id', remove)   
+    .delete('/destroy/:id',destroy)
 
 module.exports = router;
