c96c2499b23f6dcdf4e52d3ee95e229515515011
jmattias1 <jmattias1@hotmail.com>
2022-10-18 17:23:41 -0300

    new_home_DB

--- New_Home_Db.sql
+++ New_Home_Db.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
 --
--- Host: localhost    Database: new_home
+-- Host: localhost    Database: new_home_16_db
 -- ------------------------------------------------------
 -- Server version	8.0.30
 
@@ -16,34 +16,35 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `cart_orders`
+-- Table structure for table `addresses`
 --
 
-DROP TABLE IF EXISTS `cart_orders`;
+DROP TABLE IF EXISTS `addresses`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `cart_orders` (
+CREATE TABLE `addresses` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `date` datetime DEFAULT NULL,
-  `total` int DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `province` varchar(255) DEFAULT NULL,
   `userId` int DEFAULT NULL,
-  `createdAt` datetime DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
   `updatedAt` datetime DEFAULT NULL,
   `deletedAt` datetime DEFAULT NULL,
-  `cart_orderscol` varchar(45) COLLATE utf8mb3_czech_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `order_user_idx` (`userId`),
-  CONSTRAINT `order_user` FOREIGN KEY (`userId`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_czech_ci;
+  KEY `userId` (`userId`),
+  CONSTRAINT `addresses_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `cart_orders`
+-- Dumping data for table `addresses`
 --
 
-LOCK TABLES `cart_orders` WRITE;
-/*!40000 ALTER TABLE `cart_orders` DISABLE KEYS */;
-/*!40000 ALTER TABLE `cart_orders` ENABLE KEYS */;
+LOCK TABLES `addresses` WRITE;
+/*!40000 ALTER TABLE `addresses` DISABLE KEYS */;
+INSERT INTO `addresses` VALUES (1,'Argentina','Escobar','Buenos Aires',1,'2022-10-17 01:55:49',NULL,NULL),(2,'Argentina','Escobar','Buenos Aires',2,'2022-10-17 01:55:49',NULL,NULL);
+/*!40000 ALTER TABLE `addresses` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -57,15 +58,15 @@ CREATE TABLE `carts` (
   `id` int NOT NULL AUTO_INCREMENT,
   `orderId` int DEFAULT NULL,
   `productId` int DEFAULT NULL,
-  `createdAt` datetime DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
   `updatedAt` datetime DEFAULT NULL,
   `deletedAt` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `cart_order_idx` (`orderId`),
-  KEY `cart_product_idx` (`productId`),
-  CONSTRAINT `cart_order` FOREIGN KEY (`orderId`) REFERENCES `cart_orders` (`id`),
-  CONSTRAINT `cart_product` FOREIGN KEY (`productId`) REFERENCES `products` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_czech_ci;
+  KEY `orderId` (`orderId`),
+  KEY `productId` (`productId`),
+  CONSTRAINT `carts_ibfk_1` FOREIGN KEY (`orderId`) REFERENCES `orders` (`id`),
+  CONSTRAINT `carts_ibfk_2` FOREIGN KEY (`productId`) REFERENCES `products` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -86,12 +87,12 @@ DROP TABLE IF EXISTS `categories`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `categories` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb3_czech_ci NOT NULL,
-  `createdAt` datetime DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
   `updatedAt` datetime DEFAULT NULL,
   `deletedAt` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_czech_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -100,7 +101,68 @@ CREATE TABLE `categories` (
 
 LOCK TABLES `categories` WRITE;
 /*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (1,'Televisores','2022-10-17 01:55:49',NULL,NULL),(2,'Consolas','2022-10-17 01:55:49',NULL,NULL),(3,'Audio','2022-10-17 01:55:49',NULL,NULL),(4,'Celulares','2022-10-17 01:55:49',NULL,NULL),(5,'Electrodomesticos','2022-10-17 01:55:49',NULL,NULL),(6,'Muebles','2022-10-17 01:55:49',NULL,NULL);
 /*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `images`
+--
+
+DROP TABLE IF EXISTS `images`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `images` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `file` varchar(255) DEFAULT NULL,
+  `productId` int DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  `deletedAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `productId` (`productId`),
+  CONSTRAINT `images_ibfk_1` FOREIGN KEY (`productId`) REFERENCES `products` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `images`
+--
+
+LOCK TABLES `images` WRITE;
+/*!40000 ALTER TABLE `images` DISABLE KEYS */;
+INSERT INTO `images` VALUES (1,'secarropas.jpg',1,'2022-10-17 01:55:49',NULL,NULL),(2,'heladera.jpg',2,'2022-10-17 01:55:49',NULL,NULL),(3,'lavarropas2.jpg',3,'2022-10-17 01:55:49',NULL,NULL),(4,'ropero.jpg',4,'2022-10-17 01:55:49',NULL,NULL),(5,'ventilador.jpg',5,'2022-10-17 01:55:49',NULL,NULL),(6,'colchon.jpg',6,'2022-10-17 01:55:49',NULL,NULL),(7,'cocina.jpg',7,'2022-10-17 01:55:49',NULL,NULL),(8,'inodoro.jpg',8,'2022-10-17 01:55:49',NULL,NULL),(9,'tostadora.png',9,'2022-10-17 01:55:49',NULL,NULL),(10,'tacho.jpg',10,'2022-10-17 01:55:49',NULL,NULL),(11,'sillon.jpg',11,'2022-10-17 01:55:49',NULL,NULL),(12,'tv1.jpg',12,'2022-10-17 01:55:49',NULL,NULL),(13,'product-1662002525488.jpg',13,'2022-10-17 01:55:49',NULL,NULL),(14,'product-1662078677579.jpg',14,'2022-10-17 01:55:49',NULL,NULL),(15,'product-1662143951330.jpg',15,'2022-10-17 01:55:49',NULL,NULL);
+/*!40000 ALTER TABLE `images` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `date` datetime DEFAULT NULL,
+  `total` int DEFAULT NULL,
+  `userId` int DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  `deletedAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `userId` (`userId`),
+  CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -112,18 +174,21 @@ DROP TABLE IF EXISTS `products`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `products` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) COLLATE utf8mb3_czech_ci NOT NULL,
-  `price` int NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `price` int DEFAULT NULL,
   `discount` int DEFAULT NULL,
-  `description` text COLLATE utf8mb3_czech_ci NOT NULL,
+  `description` text,
   `categoryId` int DEFAULT NULL,
-  `createdAt` datetime DEFAULT NULL,
+  `subcategoryId` int DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
   `updatedAt` datetime DEFAULT NULL,
   `deletedAt` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `products_category_idx` (`categoryId`),
-  CONSTRAINT `products_category` FOREIGN KEY (`categoryId`) REFERENCES `categories` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_czech_ci;
+  KEY `categoryId` (`categoryId`),
+  KEY `subcategoryId` (`subcategoryId`),
+  CONSTRAINT `products_ibfk_1` FOREIGN KEY (`categoryId`) REFERENCES `categories` (`id`),
+  CONSTRAINT `products_ibfk_2` FOREIGN KEY (`subcategoryId`) REFERENCES `subcategories` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -132,7 +197,86 @@ CREATE TABLE `products` (
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (1,'Secarropas centrífugo Koh-i-noor',35999,10,'lorem ipsum dolor amet sit.',5,NULL,'2022-10-17 01:55:49',NULL,NULL),(2,'Heladera inverter no frost Samsung',158190,15,'lorem ipsum dolor amet sit.',5,NULL,'2022-10-17 01:55:49',NULL,NULL),(3,'Lavarropas automático Whirlpool',100499,20,'lorem ipsum dolor amet sit.',5,NULL,'2022-10-17 01:55:49',NULL,NULL),(4,'Ropero de madera',45065,10,'lorem ipsum dolor amet sit.',6,NULL,'2022-10-17 01:55:49',NULL,NULL),(5,'Ventilador de techo Liliana',44899,15,'lorem ipsum dolor amet sit.',5,NULL,'2022-10-17 01:55:49',NULL,NULL),(6,'Colchón 1 plaza de espuma',47599,20,'lorem ipsum dolor amet sit.',6,NULL,'2022-10-17 01:55:49',NULL,NULL),(7,'Cocina Electrolux',189799,30,'lorem ipsum dolor amet sit.',5,NULL,'2022-10-17 01:55:49',NULL,NULL),(8,'Tostadora Moulinex Vita',7590,35,'lorem ipsum dolor amet sit.',5,NULL,'2022-10-17 01:55:49',NULL,NULL),(9,'Inodoro Prunus One Piece',37012,30,'lorem ipsum dolor amet sit.',6,NULL,'2022-10-17 01:55:49',NULL,NULL),(10,'Cesto Tacho Basura Acero Inoxidable Pedal',4990,50,'lorem ipsum dolor amet sit.',6,NULL,'2022-10-17 01:55:49',NULL,NULL),(11,'Sillon Capitone Premium',19999,40,'lorem ipsum dolor amet sit.',6,NULL,'2022-10-17 01:55:49',NULL,NULL),(12,'Smart TV Samsung',112999,40,'lorem ipsum dolor amet sit.',1,NULL,'2022-10-17 01:55:49',NULL,NULL),(13,'Sony PlayStation 5 825GB Standard color blanco y negro',310000,20,'Con tu consola PlayStation 5 tendrás entretenimiento asegurado todos los días. Su tecnología fue creada para poner nuevos retos tanto a jugadores principiantes como expertos.\r\n\r\nPlayStation renovó las expectativas del mundo virtual con esta nueva consola y su gran rendimiento. Cuenta con una interfaz de usuario más rápida y fácil de navegar que en anteriores modelos. Además, podrás jugar durante horas desafiando a millones de contrincantes alrededor del mundo que esperan nuevos retos.\r\n\r\nCalidad de otro nivel\r\nSe considera que esta consola es de las mejores dentro del mercado, dado que presenta una resolución de hasta 4K.',2,NULL,'2022-10-17 01:55:49',NULL,NULL),(14,'Sega genesis',5000,0,'Sega Mega Drive o Sega Genesis es una videoconsola de sobremesa producida por SEGA, lanzada al mercado en 1988. Esta videoconsola es la sucesora directa de la Sega Master System y compitió contra la SNES de Nintendo, como parte de las videoconsolas de cuarta generación.',2,NULL,'2022-10-17 01:55:49',NULL,NULL),(15,'Iphone 12',250000,5,'Esta re bueno',4,NULL,'2022-10-17 01:55:49',NULL,NULL);
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rols`
+--
+
+DROP TABLE IF EXISTS `rols`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `rols` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  `deletedAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rols`
+--
+
+LOCK TABLES `rols` WRITE;
+/*!40000 ALTER TABLE `rols` DISABLE KEYS */;
+INSERT INTO `rols` VALUES (1,'admin','2022-10-17 01:55:49',NULL,NULL),(2,'user','2022-10-17 01:55:49',NULL,NULL);
+/*!40000 ALTER TABLE `rols` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sequelizemeta`
+--
+
+DROP TABLE IF EXISTS `sequelizemeta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sequelizemeta` (
+  `name` varchar(255) COLLATE utf8mb3_unicode_ci NOT NULL,
+  PRIMARY KEY (`name`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sequelizemeta`
+--
+
+LOCK TABLES `sequelizemeta` WRITE;
+/*!40000 ALTER TABLE `sequelizemeta` DISABLE KEYS */;
+INSERT INTO `sequelizemeta` VALUES ('20221014211934-create-sub-category.js'),('20221014212106-create-category.js'),('20221014212123-create-rol.js'),('20221014212217-create-product.js'),('20221014212239-create-image.js'),('20221014212335-create-user.js'),('20221014212433-create-address.js'),('20221014212505-create-order.js'),('20221014212543-create-cart.js');
+/*!40000 ALTER TABLE `sequelizemeta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `subcategories`
+--
+
+DROP TABLE IF EXISTS `subcategories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `subcategories` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  `deletedAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `subcategories`
+--
+
+LOCK TABLES `subcategories` WRITE;
+/*!40000 ALTER TABLE `subcategories` DISABLE KEYS */;
+INSERT INTO `subcategories` VALUES (1,'Sony','2022-10-17 01:55:49',NULL,NULL),(2,'Microsoft','2022-10-17 01:55:49',NULL,NULL),(3,'Wii','2022-10-17 01:55:49',NULL,NULL),(4,'Dream','2022-10-17 01:55:49',NULL,NULL),(5,'LG','2022-10-17 01:55:49',NULL,NULL),(6,'Motorola','2022-10-17 01:55:49',NULL,NULL),(7,'Samsung','2022-10-17 01:55:49',NULL,NULL),(8,'Hp','2022-10-17 01:55:49',NULL,NULL),(9,'Mac','2022-10-17 01:55:49',NULL,NULL),(10,'Acer','2022-10-17 01:55:49',NULL,NULL),(11,'philips','2022-10-17 01:55:49',NULL,NULL);
+/*!40000 ALTER TABLE `subcategories` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -144,21 +288,19 @@ DROP TABLE IF EXISTS `users`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb3_czech_ci NOT NULL,
-  `surname` varchar(255) COLLATE utf8mb3_czech_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb3_czech_ci NOT NULL,
-  `password` varchar(32) COLLATE utf8mb3_czech_ci NOT NULL,
-  `country` varchar(255) COLLATE utf8mb3_czech_ci DEFAULT NULL,
-  `avatar` varchar(255) COLLATE utf8mb3_czech_ci DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `surname` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `avatar` varchar(255) DEFAULT NULL,
   `rolId` int DEFAULT NULL,
-  `createdAt` datetime DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
   `updatedAt` datetime DEFAULT NULL,
   `deletedAt` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `email_UNIQUE` (`email`),
-  KEY `users_rol_idx` (`rolId`),
-  CONSTRAINT `users_rol` FOREIGN KEY (`rolId`) REFERENCES `users_rol` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_czech_ci;
+  KEY `rolId` (`rolId`),
+  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`rolId`) REFERENCES `rols` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -167,33 +309,8 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Roman','Riquelme','hola@gmail.com','$2a$10$iRE5bBRyPZNREwCVwMLfHOhnFb3v2Js7KDes4j8q1zXv78jfYpqQu','1662079974191_img.jpg',1,'2022-10-17 01:55:49',NULL,NULL),(2,'Goku','Kakaroto','chau014@gmail.com','$2a$10$UculHn2j.Jtc6u/DeQGQcOmg2gdkPTKGFkqifkrSkB32yBu/Qn8.S','1662502140409_img.jpg',1,'2022-10-17 01:55:49',NULL,NULL);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `users_rol`
---
-
-DROP TABLE IF EXISTS `users_rol`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users_rol` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb3_czech_ci DEFAULT NULL,
-  `createdAt` datetime DEFAULT NULL,
-  `updatedAt` datetime DEFAULT NULL,
-  `deletedAt` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_czech_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users_rol`
---
-
-LOCK TABLES `users_rol` WRITE;
-/*!40000 ALTER TABLE `users_rol` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users_rol` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -205,4 +322,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-10-03 17:21:59
+-- Dump completed on 2022-10-18 17:21:53
