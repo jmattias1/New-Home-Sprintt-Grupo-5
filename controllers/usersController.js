f6af066e02e41b512f8945b49c48257cfa6d4c41
Danimartinez25 <danimartinez1325@gmail.com>
2022-10-25 13:56:43 -0300

    procees login

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,24 +1,25 @@
     const bcryptjs = require ('bcryptjs');
 const {validationResult} = require('express-validator');
+const db = require('../database/models')
 
 /* User de models */
 const User = require ('../models/User');
 
 module.exports = {
-
-    register : (req,res) => {
-        return res.render('register',{
-            title: 'Register'
-        })
-    },
+  register: (req, res) => {
+    return res.render("register", {
+      title: "Register",
+    });
+  },
 
   processRegister: (req, res) => {
-    const { name, surname, password, email, country, province, city,rolId} =req.body;
+    const { name, surname, password, email, country, province, city, rolId } =
+      req.body;
     let rol = db.Rol.findAll({
       attributes: ["id", "name"],
       order: ["name"],
     });
-    const {id} = db.User.create({
+    const { id } = db.User.create({
       name: name?.trim(),
       surname: surname?.trim(),
       email: email?.trim(),
@@ -29,74 +30,71 @@ module.exports = {
       avatar: req.file?.filename,
       rolId: rolId,
     });
-    
+
     let addressCreate = db.Address.create({
       country: country?.trim(),
       city: city?.trim(),
       province: province?.trim(),
       userId: id,
     });
-    Promise.all([addressCreate, rol]).then(
-      ([addressCreate, rol]) => {
+    Promise.all([addressCreate, rol]).then(([addressCreate, rol]) => {
       ({
         addressCreate,
-        rol
-      })
-      return res.redirect('profile')
-      }
-      
-    );
+        rol,
+      });
+      return res.redirect("profile");
+    });
+  },
+  login: (req, res) => {
+    return res.render("login");
   },
 
-    login : (req,res) => {
-        return res.render('login',{
-            title: 'Login'
-        })
-    },
-
-    loginProcess: (req,res) => {
-        let userToLogin = User.findByTag('email', req.body.email);
-
-        if (userToLogin) {
-            let isCorrectPassword = bcryptjs.compareSync (req.body.password , userToLogin.password);
-            if (isCorrectPassword){
-                delete userToLogin.password;
-                req.session.userLogged = userToLogin;
-
-                if (req.body.remember_user) {
-                    res.cookie ('userEmail' , req.body.email, {maxAge: (1000 * 60) * 2});
-                }
-                return res.redirect ('/users/profile')
-            }
-            return res.render ('login', {
-                title: 'Login',
-                errors: {
-                    email: {
-                        msg : 'las creedenciales son invalidas'
-                    }
-                }
-            });
-        }
-
-        return res.render ('login', {
-            title: 'Login',
-            errors: {
-                email: {
-                    msg : 'Este email no se encuentra en nuestra base de datos'
-                }
-            }
+  loginProcess: (req, res) => {
+    const { email } = req.body;
+    db.User.findOne({
+      where: {
+        email,
+      },
+    }).then((user) => {
+      req.session.userLogin = {
+        id: user.id,
+        name: user.name,
+        surname: user.surname,
+        email: user.email,
+        rol: user.rolId,
+        avatar: user.avatar,
+      };
+      if (req.body.remember) {
+        res.cookie("newHome", req.session.userLogin, {
+          maxAge: 1000 * 60 * 60 * 24,
         });
-    },
+      }
+      res.locals.user = req.session.userLogin;
+      return res.redirect("/");
+    });
+  },
 
-    profile: (req,res) => {
-        return res.render ('userProfile', {
-            title: 'Perfil',
-            user: req.session.userLogged
-        })
-    },
-    logout : (req,res) => {
-        res.clearCookie('userEmail');
-        req.session.destroy();
-        return res.redirect('/');
-    }
-}
+  profile: (req, res) => {
+    const id = req.session.userLogin?.id;
+    db.User.findByPk(id)
+      .then((user) => {
+        return res.render("profile", {
+          title: "New Home Perfil",
+          user,
+        });
+      })
+      .catch((err) => console.log(err));
+    /*     return res.render("users/profile", {
+      title: "Perfil",
+      user: req.session.userLogged,
+    }); */
+  },
+  logout: (req, res) => {
+    req.session.destroy();
+    res.cookie("newHome", null, { maxAge: -1 });
+    return res.redirect("/");
+  },
+  logoutV: (req, res) => {
+    return res.render("logout");
+  },
+};
